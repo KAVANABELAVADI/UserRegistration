1381240bab119ea961d163c4bce81e3b4868065e
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-23 16:31:04 +0530

    Email ID

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,13 +2,20 @@
 
 read -p "Enter User First Name - " firstName
 read -p "Enter User Last Name - " lastName
+read -p "Enter Email ID - " email
 NamePat="^[A-Z]{1}[a-z]{2,6}$"
+emailPat="^0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-z]{2,4}([.][a-zA-Z]{2}+)*$"
 
 if [[ $firstName =~ $NamePat ]]
 then
 	if [[ $lastName =~ $NamePat ]]
 	then
-		echo "Valid Last Name"
+		if [[ $email =~ $emailPat ]]
+		then
+			echo "Valid Email ID"
+		else
+			echo "Enter Valid Email ID"
+		fi
 	else
 		echo "Enter Valid Last Name"
 	fi
